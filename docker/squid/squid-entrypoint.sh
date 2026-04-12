3898f392a7039bb04c0009055f7c50a0aa6bef47
Imanuel Bertrand <imanuel@bertrand.one>
2026-04-12 21:20:03 +0200

    Improve access to upstream manictime

--- docker/squid/squid-entrypoint.sh
+++ docker/squid/squid-entrypoint.sh
@@ -5,7 +5,8 @@ set -e
 # (it won't be in dev, where squid-dev.conf is mounted read-only).
 if [ -w /etc/squid/squid.conf ]; then
     MT_HOSTNAME="${MT_HOSTNAME:-example.invalid}"
-    envsubst '${MT_HOSTNAME}' < /etc/squid/squid.conf.template > /etc/squid/squid.conf
+    MT_PORT="${MT_PORT:-80}"
+    envsubst '${MT_HOSTNAME} ${MT_PORT}' < /etc/squid/squid.conf.template > /etc/squid/squid.conf
 fi
 
 exec squid -NYC --foreground
